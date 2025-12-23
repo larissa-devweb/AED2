4110105f7582b37434ef1a2bd38e2ffb6c01c9af
Larissa <82511298+larissa-devweb@users.noreply.github.com>
2025-12-23 18:52:01 -0300

    Add files via upload

---
+++ Codificação de Huffman/LARISSA_huffman.c
@@ -0,0 +1,27 @@
+//LARISSA GABRIELA - M2
+
+/*PRECISO DE:
+ler arquivos em byte ou caracter por caracter
+contar frequências
+construir árvore de huffman
+gerar códigos binários, se usei bytes
+compactar , salvar, descompactar usando a árvore/tabela
+comparar .txt final com o original
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <stdint.h>
+//#include <stdbool.h>
+
+
+
+
+
+int main() {
+    
+    compactar("palavras.txt", "compactado.huff");
+    descompactar("compactado.huff", "saida.txt");
+    return 0;
+}
